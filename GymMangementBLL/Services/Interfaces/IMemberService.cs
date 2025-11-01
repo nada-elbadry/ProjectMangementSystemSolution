9d290fa7e6846fe584e252ca91a372ed0fc64deb
Your Name <Nada Elbadry nadaelbadry1012@gmail.com>
2025-11-01 14:08:45 +0200

    Task

---
+++ GymMangementBLL/Services/Interfaces/IMemberService.cs
@@ -0,0 +1,34 @@
+﻿using GymMangementBLL.ViewModels.MemberViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangementBLL.Services.Interfaces
+{
+    internal interface IMemberService
+    { 
+        #region Main CRUD Methods
+        IEnumerable<MemberViewModel> GetAllMembers();
+        bool CreateMember(CreatMemberViewModel createMember);
+        MemberViewModel? GetMemberDeails(int MemberId);
+ 
+        MemberToUpdateViewModel? GetMemberToUpdate(int MemberId);
+        bool UpdateMemberDetails(int Id, MemberToUpdateViewModel memberToUpdate);
+        bool RemoveMember(int MemberId);
+    
+
+        HealthRecordViewModel? GetMemberHealthRecordDetails(int MemberId);
+        #endregion
+
+        #region Validation & Helper Methods
+        bool IsEmailExists(string Email);
+        bool IsPhoneExists(string Phone);
+        bool IsEmailExists(string Email, int memberIdToExclude);
+        bool IsPhoneExists(string Phone, int memberIdToExclude);
+        bool HasActiveSessions(int MemberId);
+        #endregion
+
+    }
+}
