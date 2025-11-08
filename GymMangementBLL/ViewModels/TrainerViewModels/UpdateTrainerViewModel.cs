23a99aa1904b2d0eb8affc3592041e5a56778b77
Your Name <Nada Elbadry nadaelbadry1012@gmail.com>
2025-11-09 00:30:25 +0200

    EDIT01

---
+++ GymMangementBLL/ViewModels/TrainerViewModels/UpdateTrainerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangementBLL.ViewModels.TrainerViewModels
+{
+    internal class UpdateTrainerViewModel
+    {
+    }
+}
---
+++ ProjectMangementPL/Controllers/SessionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectMangementPL.Controllers
+{
+    public class SessionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
